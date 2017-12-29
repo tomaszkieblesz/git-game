07cc7ea976ed2203ae10e60f6e238b4ee7e65bfe
tomaszkieblesz <tomasz.kieblesz@gmail.com>
2017-12-29 12:02:50 +0100

    Pygame na dziś
    Tutorial z yt

--- lets-game.py
+++ lets-game.py
@@ -1,8 +1,17 @@
-import matplotlib
+import matplotlib, sys
 
 import requests
 
 import pygame
 
 print(pygame.__version__)
+screen = pygame.display.set_mode((400, 300))
 
+while True:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            sys.exit(0)
+
+    # Rysowanie
+    pygame.draw.rect(screen, (20, 100, 255), pygame.Rect(10, 50, 200, 100))
+    pygame.display.flip()
