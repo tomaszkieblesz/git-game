d8e8ef726b23f1b9f18eae0fd06ba28289fbb0fc
tomaszkieblesz <tomasz.kieblesz@gmail.com>
2017-12-29 12:04:45 +0100

    Dodane komentarze

--- lets-game.py
+++ lets-game.py
@@ -7,11 +7,13 @@ import pygame
 print(pygame.__version__)
 screen = pygame.display.set_mode((400, 300))
 
+
+# Pętla nasłuchująca
 while True:
     for event in pygame.event.get():
         if event == pygame.QUIT:
             sys.exit(0)
 
-    # Rysowanie
+    # Rysowanie kwadratu
     pygame.draw.rect(screen, (20, 100, 255), pygame.Rect(10, 50, 200, 100))
     pygame.display.flip()
