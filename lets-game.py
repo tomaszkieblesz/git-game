8e2b57ef8a7a1359f6c2061be45755ca6349475e
io3times <io3times@gmail.com>
2018-01-02 22:29:02 +0100

    Random

--- lets-game.py
+++ lets-game.py
@@ -2,9 +2,9 @@ import pygame
 import sys
 
 pygame.init()
-window = pygame.display.set_mode((600, 400))
+window = pygame.display.set_mode((600, 400), 0, 32)
 screen=pygame.display.get_surface()
-kwadrat=pygame.image.load('images/ship.bmp')
+kwadrat=pygame.image.load('images/ship.bmp').convert()
 
 kwadrat.set_alpha(10)
 screen.fill((120,120,120))
