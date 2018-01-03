8102b0d89e930dc57ac7ec947d4e8408845ca87f
io3times <io3times@gmail.com>
2018-01-03 22:14:01 +0100

    Ile liter w tekście

--- lets-game.py
+++ lets-game.py
@@ -1,19 +1,34 @@
-#Czy gałąź pro jest aktywna?
-#from string import ascii_letters
+sentencja=input("Wpisz zdanie i naciśnij ENTER:")
 
+sent=sentencja.lower().strip()
 
-sentencja=input()
+ALFABET="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMąĄęĘóÓłńŃŁŻŹćżźśŚ"
 
-sent=sentencja.lower()
-
-ALFABET="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMęĘóÓłńŃŁŻŹćżźśŚ"
-
+mlf=[]
 for x in sent:
     if x in (ALFABET):
-        mfl="".join(x)
-        print(mfl)
+        mlf.append(x)
+#print(mlf)
+baza=[]
 
+alfa=[]
+for i in range(mlf.__len__()):
+    l=mlf[i]
+    litery=mlf.count(l)
+    if [litery, l] not in baza:
+        baza.append([litery,l])
 
+srt=sorted(baza)
+#print(srt)
+#print(srt[-1])
+if srt[-1][0]==srt[-2][0]:
+    a=mlf.index(srt[-1][1])
+    b=mlf.index(srt[-2][1])
+    y=min(a,b)
+    #print(y)
+    print("Najliczniej występującą literą w tekście jest: "+sent[y])
+else:
+    print("Najliczniej występującą literą w tekście jest: "+srt[-1][1])
 
 
 
