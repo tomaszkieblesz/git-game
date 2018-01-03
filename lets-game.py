e5c8f37b4678603d11fe0aebd6502f2b861d5896
tomaszkieblesz <tomasz.kieblesz@gmail.com>
2018-01-03 14:20:18 +0100

    CheckIO

--- lets-game.py
+++ lets-game.py
@@ -1,1 +1,19 @@
 #Czy gałąź pro jest aktywna?
+#from string import ascii_letters
+
+sentencja=input()
+
+sent=sentencja.lower()
+
+ALFABET="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMęĘóÓłńŃŁŻŹćżźśŚ"
+
+for x in sent:
+    if x in (ALFABET):
+        mfl="".join(x)
+        print(mfl)
+
+
+
+
+
+
